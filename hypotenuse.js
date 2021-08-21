c4abdb94c8dd471e9b19bf1b04147106271726d2
Mradul Agrawal <mradulagrawal126@gmail.com>
2021-08-21 14:11:00 +0530

    hypotenuse calc done

---
+++ hypotenuse.js
@@ -0,0 +1,16 @@
+const side1 = document.querySelector('#side1');
+const side2 = document.querySelector('#side2');
+const output = document.querySelector('.outputDiv');
+const btn = document.querySelector("#submitBtn");
+
+
+function clickHandler(e){
+    e.preventDefault()
+    sumOfSquare = side1.value**2 + side2.value**2
+    hypotenuse = Math.sqrt(sumOfSquare)
+    output.innerText = hypotenuse
+
+}
+
+
+btn.addEventListener("click",clickHandler)
